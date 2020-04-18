5375e468e9bd3940e2eb6e5ba1742827263b469d
jonndoe <qb89@yandex.ru>
2020-04-19 01:51:42 +0300

    ch3

--- main.py
+++ main.py
@@ -34,16 +34,59 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = self.fc4(x)
-        return x
+
+        return F.log_softmax(x, dim=1)
+
 
 net = Net()
 
-x = torch.randn((28, 28)) # create some sample test data just for example
-x = x.view(-1, 28*28)   # always use -1 when flatten in pytorch
-                        # that means "any input shape"
-print(net)
-print(x)
+import torch.optim as optim
 
-output = net(x)
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-print(output)
+
+# train the nn
+for epoch in range(3): # 3 full passes over the data
+    for data in trainset:  # `data` is a batch of data
+        X, y = data  # X is the batch of features, y is the batch of targets.
+        net.zero_grad()  # sets gradients to 0 before loss calc. You will do this likely every step.
+        output = net(X.view(-1,784))  # pass in the reshaped batch (recall they are 28x28 atm)
+        loss = F.nll_loss(output, y)  # calc and grab the loss value
+        loss.backward()  # apply this loss backwards thru the network's parameters
+        optimizer.step()  # attempt to optimize weights to account for loss/gradients
+    print(loss)  # print loss. We hope loss (a measure of wrong-ness) declines!
+
+
+correct = 0
+total = 0
+with torch.no_grad(): # dont calculate gradients
+    for data in testset:
+        X, y = data
+        output = net(X.view(-1,784))
+        #print(output)
+        for idx, i in enumerate(output):
+            #print(torch.argmax(i), y[idx])
+            if torch.argmax(i) == y[idx]:
+                correct += 1
+            total += 1
+
+print("Accuracy: ", round(correct/total, 3))
+
+
+import matplotlib.pyplot as plt
+
+plt.imshow(X[0].view(28,28))
+plt.show()
+
+print(torch.argmax(net(X[0].view(-1,784))[0]))
+
+
+# above in details:
+a_featureset = X[0]
+reshaped_for_network = a_featureset.view(-1,784) # 784 b/c 28*28 image resolution.
+output = net(reshaped_for_network) #output will be a list of network predictions.
+first_pred = output[0]
+print(first_pred)
+biggest_index = torch.argmax(first_pred)
+print(biggest_index)
