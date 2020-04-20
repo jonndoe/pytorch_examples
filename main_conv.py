0b6f5de971ea48ff6feaede772c3300713c40a88
jonndoe <qb89@yandex.ru>
2020-04-20 12:15:03 +0300

    pyplot added, works

--- main_conv.py
+++ main_conv.py
@@ -210,3 +210,38 @@ def batch_test(net):
 batch_test(net)
 
 
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use("ggplot")
+
+model_name = "model-1587331370"
+
+
+def create_acc_loss_graph(model_name):
+    contents = open("model.log", "r").read().split("\n")
+
+    times = []
+    accuracies = []
+    losses = []
+
+    for c in contents:
+        if model_name in c:
+            name, timestamp, sample_type, acc, loss = c.split(",")
+
+            times.append(timestamp)
+            accuracies.append(acc)
+            losses.append(loss)
+
+    fig = plt.figure()
+
+    ax1 = plt.subplot2grid((2,1), (0,0))
+    ax2 = plt.subplot2grid((2,1), (1,0), sharex=ax1)
+
+    ax1.plot(times, accuracies, label="in_samp_acc")
+    ax1.legend(loc=2)
+    ax2.plot(times, losses, label="in_samp_loss")
+    ax2.legend(loc=2)
+    plt.show()
+
+create_acc_loss_graph(model_name)
