90051cee4310734979bae772881460cdd2bb3c6f
jonndoe <qb89@yandex.ru>
2020-08-02 20:59:03 +0300

    added simplest nn model

--- main_2.py
+++ main_2.py
@@ -17,10 +17,53 @@ trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
 testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=False)
 
 
-
+'''
 print(train[5][1])
 
 import matplotlib.pyplot as plt  # pip install matplotlib
 
 plt.imshow(train[5][0].view(28,28))
 plt.show()
+
+
+total = 0
+counter_dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+
+
+for data in trainset:
+    Xs, ys = data
+    for y in ys:
+        counter_dict[int(y)] += 1
+        total += 1
+
+print(counter_dict)
+
+for i in counter_dict:
+    print(f"{i}: {counter_dict[i]/total*100.0}%")
+'''
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # first param is INPUT size,
+        # second param is OUTPUT size
+        self.fc1 = nn.Linear(28*28, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 64)
+        self.fc4 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        x = self.fc4(x)
+        return x
+
+net = Net()
+print(net)
+
+
+
