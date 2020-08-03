8863ab814a596df898950483857a0d2d1238fcc9
jonndoe <qb89@yandex.ru>
2020-08-03 15:47:13 +0300

    simple nn works

--- main_2.py
+++ main_2.py
@@ -2,6 +2,8 @@ import torch
 import torchvision
 from torchvision import transforms, datasets
 
+import torch.optim as optim
+
 train = datasets.MNIST('', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor()
@@ -17,30 +19,6 @@ trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
 testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=False)
 
 
-'''
-print(train[5][1])
-
-import matplotlib.pyplot as plt  # pip install matplotlib
-
-plt.imshow(train[5][0].view(28,28))
-plt.show()
-
-
-total = 0
-counter_dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-
-
-for data in trainset:
-    Xs, ys = data
-    for y in ys:
-        counter_dict[int(y)] += 1
-        total += 1
-
-print(counter_dict)
-
-for i in counter_dict:
-    print(f"{i}: {counter_dict[i]/total*100.0}%")
-'''
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -65,6 +43,49 @@ class Net(nn.Module):
 net = Net()
 print(net)
 
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+for epoch in range(3): # 3 full passes over the data
+    for data in trainset:  # `data` is a batch of data
+        X, y = data  # X is the batch of features, y is the batch of targets.
+        net.zero_grad()  # sets gradients to 0 before loss calc. You will do this likely every step.
+        output = net(X.view(-1,784))  # pass in the reshaped batch (recall they are 28x28 atm)
+        loss = F.nll_loss(output, y)  # calc and grab the loss value
+        loss.backward()  # apply this loss backwards thru the network's parameters
+        optimizer.step()  # attempt to optimize weights to account for loss/gradients
+    print(loss)  # print loss. We hope loss (a measure of wrong-ness) declines!
+
+# CALCULATE ACCURACY:
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in testset:
+        X, y = data
+        output = net(X.view(-1,784))
+        #print(output)
+        for idx, i in enumerate(output):
+            #print(torch.argmax(i), y[idx])
+            if torch.argmax(i) == y[idx]:
+                correct += 1
+            total += 1
+
+print("Accuracy: ", round(correct/total, 3))
+
+
+
+
+
+
+
+
+
+
+
+'''
+
 # for testing nn create random image
 X = torch.randn((28, 28))
 # flatten it for input
@@ -75,6 +96,40 @@ X = X.view(-1, 28*28)
 output = net(X)
 
 print(output)
+
+
+
+
+
+
+
+
+
+print(train[5][1])
+
+import matplotlib.pyplot as plt  # pip install matplotlib
+
+plt.imshow(train[5][0].view(28,28))
+plt.show()
+
+
+total = 0
+counter_dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+
+
+for data in trainset:
+    Xs, ys = data
+    for y in ys:
+        counter_dict[int(y)] += 1
+        total += 1
+
+print(counter_dict)
+
+for i in counter_dict:
+    print(f"{i}: {counter_dict[i]/total*100.0}%")
+'''
+
+
 
 
 
